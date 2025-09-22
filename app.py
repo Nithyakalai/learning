fc608df271a8d1ec58ad466d4a6dabf3c8a1cee0
Nithyakalai <nithyakalaichelvan907@gmail.com>
2025-09-22 22:20:20 +0530

    Flask connected with css

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route("/")
-def hello():
-    return "hello nithya"
-if __name__== '__main__':
-    app.run(host='0.0.0.0',debug='True')
+def home():
+    return render_template("homepage.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
            
